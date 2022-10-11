23978d7d8736496f17a4715b5d4f35e8577e0a37
Tom <git@tomr.me>
2022-10-11 21:46:02 +0100

    more linebreak stuff (where did i get \nb from what)

--- rtl88x2bu.sh
+++ rtl88x2bu.sh
@@ -8,20 +8,20 @@ fi
 
 # Stolen from the doc on Teams, sorry Neil xx
 
-echo -e "\nb\nb##########\nb\nbUpdating Package Repos & Upgrading"
+echo -e "\n\n##########\n\nUpdating Package Repos & Upgrading"
 apt update -y && apt upgrade -y
 
-echo -e "\nb\nb##########\nb\nbInstalling aircrack-ng, git & dksm"
+echo -e "\n\n##########\n\nInstalling aircrack-ng, git & dksm"
 apt install aircrack-ng dkms git -y
 
-echo -e "\nb\nb##########\nb\nbCloning repository to /usr/src/rtl88x2bu-git"
+echo -e "\n\n##########\n\nCloning repository to /usr/src/rtl88x2bu-git"
 rm -rfv /usr/src/rtl88x2bu-git # remove if it already exists
 git clone https://github.com/RinCat/RTL88x2BU-Linux-Driver.git /usr/src/rtl88x2bu-git -v 
 
-echo -e "\nb\nb##########\nb\nbInstalling RTL88x2BU dynamic kernel module"
+echo -e "\n\n##########\n\nInstalling RTL88x2BU dynamic kernel module"
 sed -i 's/PACKAGE_VERSION="@PKGVER@"/PACKAGE_VERSION="git"/g' /usr/src/rtl88x2bu-git/dkms.conf
 dkms add -m rtl88x2bu -v git
 
 # \a = beep speaker!!
-# \nb = linebreak hopefully
-echo -e "\a\nb\nb##########\nb\nbDone with everything (hopefully)!\nYou must reboot your system and run 'dkms autoinstall' to complete install.\nbGood Luck :)"
+# \n = linebreak hopefully
+echo -e "\a\n\n##########\n\nDone with everything (hopefully)! \nYou must reboot your system and run 'dkms autoinstall' to complete install.\nGood Luck :)"
