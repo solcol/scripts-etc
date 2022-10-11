4fef49b6fbb24bd217bcf761d6fe0e42e8bab196
Tom <git@tomr.me>
2022-10-11 21:43:39 +0100

    fix some new lines

--- rtl88x2bu.sh
+++ rtl88x2bu.sh
@@ -1,26 +1,27 @@
 #!/usr/bin/env bash
  
 if [ "$EUID" -ne 0 ]; then
-    echo "This script must be ran as root! Try: 'sudo bash rtl88x2bu.sh'"
+    echo "This script must be ran as root! Try prefacing your command with 'sudo' :)"
     exit 1
 fi
 
 
 # Stolen from the doc on Teams, sorry Neil xx
 
-echo "Updating Package Repos & Upgrading"
+echo -e "\nb\nb##########\nb\nbUpdating Package Repos & Upgrading"
 apt update -y && apt upgrade -y
 
-echo "Installing aircrack-ng, git & dksm"
+echo -e "\nb\nb##########\nb\nbInstalling aircrack-ng, git & dksm"
 apt install aircrack-ng dkms git -y
 
-echo "Cloning repository to /usr/src/rtl88x2bu-git"
+echo -e "\nb\nb##########\nb\nbCloning repository to /usr/src/rtl88x2bu-git"
 rm -rfv /usr/src/rtl88x2bu-git # remove if it already exists
 git clone https://github.com/RinCat/RTL88x2BU-Linux-Driver.git /usr/src/rtl88x2bu-git -v 
 
-echo "Installing RTL88x2BU dynamic kernel module"
+echo -e "\nb\nb##########\nb\nbInstalling RTL88x2BU dynamic kernel module"
 sed -i 's/PACKAGE_VERSION="@PKGVER@"/PACKAGE_VERSION="git"/g' /usr/src/rtl88x2bu-git/dkms.conf
 dkms add -m rtl88x2bu -v git
 
-echo -e "\a" # Beep the PC speaker!
-echo "\n\nDone with everything--hopefully! You must reboot your system and run 'dkms autoinstall' to complete install.\nGood Luck :)"
+# \a = beep speaker!!
+# \nb = linebreak hopefully
+echo -e "\a\nb\nb##########\nb\nbDone with everything (hopefully)!\nYou must reboot your system and run 'dkms autoinstall' to complete install.\nbGood Luck :)"
